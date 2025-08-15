49c30c47e2af3b27a5f71408a88b1b5f229d14c3
Yash Nannaware <nannawareyash046@gmail.com>
2025-08-15 11:17:33 +0530

    Update Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -3,60 +3,71 @@ Assignment 2:- Implement the problem of moving all zeroes to the end of array.
 Statement: Given an array of random numbers, Push all the zeroes of a
 given array to the end of the array.
 */
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-void merge(vector<int> &arr,int st,int mid,int end){   // Merge function
-    vector<int>temp;
-    int i=st,j=mid+1;
-    while(i<=mid && j<=end){
-        if(arr[i] >= arr[j]){
-            temp.push_back(arr[i]);
-            i++;
-        }
-        else{
-            temp.push_back(arr[j]);
-            j++;
-        }
-      }
 
-      while(i<=mid){          // If any elements are left in the left half
-        temp.push_back(arr[i]);
-            i++;
-      }
-
-      while(j<=end){           // If any elements are left in the right half  
-        temp.push_back(arr[j]); 
-        j++;
-      }
-
-      for(int idx=0;idx<temp.size();idx++){     // Copy the sorted elements from temp back to the original array
-        arr[st+idx]=temp[idx];
-      }
-
+void merge(vector<int> &arr, int st, int mid, int end)
+{ // Merge function
+  vector<int> temp;
+  int i = st, j = mid + 1;
+  while (i <= mid && j <= end)
+  {
+    if (arr[i] >= arr[j])
+    {
+      temp.push_back(arr[i]);
+      i++;
     }
-void mergeSort(vector<int> &arr,int st,int end){
-    int mid=st+(end-st)/2;
-    if(st<end){
-       mergeSort(arr,st,mid); //left half
-       mergeSort(arr,mid+1,end); //right half
-
-       merge(arr,st,mid,end);
+    else
+    {
+      temp.push_back(arr[j]);
+      j++;
     }
+  }
+
+  while (i <= mid)
+  { // If any elements are left in the right half
+    temp.push_back(arr[i]);
+    i++;
+  }
+
+  while (j <= end)
+  { // If any elements are left in the right half
+    temp.push_back(arr[j]);
+    j++;
+  }
+
+  for (int idx = 0; idx < temp.size(); idx++)
+  { // Copy the sorted elements from temp back to the original array
+    arr[st + idx] = temp[idx];
+  }
+}
+void mergeSort(vector<int> &arr, int st, int end)
+{
+  int mid = st + (end - st) / 2;
+  if (st < end)
+  {
+    mergeSort(arr, st, mid);      // left half
+    mergeSort(arr, mid + 1, end); // right half
+
+    merge(arr, st, mid, end);
+  }
 }
 
-int main(){
-    vector<int> arr = {1,0,2,0,3,0};
+int main()
+{
+  vector<int> arr = {1, 0, 2, 0, 3, 0};
 
-    mergeSort(arr, 0, arr.size() - 1);
+  mergeSort(arr, 0, arr.size() - 1);
 
-    cout << "Sorted array: ";
-    for (int val:arr) {
-        cout << val<< " ";
-    }
-    cout << endl;
+  cout << "Sorted array: ";
+  for (int val : arr)
+  {
+    cout << val << " ";
+  }
+  cout << endl;
 
-    return 0;
+  return 0;
 }
 
 //INPUT:-vector<int> arr = {1,0,2,0,3,0};
